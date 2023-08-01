b433ce5beab4d456f9a2c76db34b722c5a21000a
reimon <reimonsk8@gmail.com>
2023-08-01 15:48:14 -0700

    instruction to launch server to web

---
+++ IceStick V2.0/RunFirst_NpmInstallServer.bat
@@ -0,0 +1,2 @@
+call npm install connect serve-static
+call node NodeServer.js
---
+++ IceStick V2.0/RunLast_HostWithngrockAccount.bat
@@ -0,0 +1,5 @@
+cd ./
+.\ngrok.exe config add-authtoken 2TOxkHNbHzaNmh61R4aq10oIOkK_5bjnU8Wr6RyXEb81cS9Gz
+.\ngrok.exe http 8080
+timeout /t 5 /nobreak
+pause
